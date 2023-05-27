4f85d63467e61e4ebd9dbce76055fcf1d3285416
mshulgina <mshulgina@cinimex.ru>
2023-05-27 17:09:34 +0500

    Добавлены тесты для класса Ingredient: тип, имя, цена ингридиента

--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,5 +1,12 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
+
+import static praktikum.IngredientType.FILLING;
+import static praktikum.IngredientType.SAUCE;
 
 public class IngredientTest {
     private Ingredient ingredient;
@@ -19,5 +26,38 @@ public class IngredientTest {
         this.expectedIngredientType = expectedIngredientType;
         this.expectedIngredientName = expectedIngredientName;
         this.expectedIngredientPrice = expectedIngredientPrice;
+    }
+    //параметризация для ингридиентов: тип, имя, цена, ожидаемый тип, ожидаемое имя, ожидаемая цена
+    @Parameterized.Parameters
+    public static Object[][] bunTestData() {
+        return new Object[][] {
+                {SAUCE, "соус", 360.20f, SAUCE, "соус", 360.20f},
+                {FILLING, "", 0, FILLING, "", 0},
+                {SAUCE, "(=^･ω･^=)", -120.50f, SAUCE, "(=^･ω･^=)", -120.50f},
+                {FILLING, "filling", 226.10f, FILLING, "filling", 226.10f}
+        };
+    }
+
+    @Before
+    public void initialize() {
+        ingredient = new Ingredient(ingredientType, ingredientName, ingredientPrice);
+    }
+
+    //тест на получение типа ингридиента
+    @Test
+    public void getTypeCorrectTest() {
+        Assert.assertEquals("Неверный тип ингридиента", expectedIngredientType, ingredient.getType());
+    }
+
+    //тест на получение имени ингридиента
+    @Test
+    public void getNameCorrectTest() {
+        Assert.assertEquals("Неверное имя ингридиента", expectedIngredientName, ingredient.getName());
+    }
+
+    //тест на получение цены ингридиента
+    @Test
+    public void getPriceCorrectTest() {
+        Assert.assertEquals("Неверная цена ингридиента", expectedIngredientPrice, ingredient.getPrice(), 0.01);
     }
 }
